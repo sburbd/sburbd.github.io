ab8d9934a70863d71bd4b2f1d43c7fb8de35b588
Zichao Zang <zichaozang@gmail.com>
2018-11-09 11:23:22 +0000

    Updates

---
+++ precache-manifest.675193187c39749ee520e48ccc28abf0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0bb0a7d9a43595ee11b6",
+    "url": "/static/js/main.0bb0a7d9.chunk.js"
+  },
+  {
+    "revision": "549149764a4abda863bc",
+    "url": "/static/js/1.54914976.chunk.js"
+  },
+  {
+    "revision": "0bb0a7d9a43595ee11b6",
+    "url": "/static/css/main.f3f22846.chunk.css"
+  },
+  {
+    "revision": "9f72e12445146877f784fc4aaaac9923",
+    "url": "/index.html"
+  }
+];
