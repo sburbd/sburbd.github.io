5f881c56ddb5cdce6014070b583b22e0cef46ad7
Zichao Zang <zichaozang@gmail.com>
2019-05-15 11:22:32 +0100

    Updates

--- precache-manifest.dd94ced0f96901d0871606f1953eff81.js
+++ precache-manifest.dd94ced0f96901d0871606f1953eff81.js
@@ -8,19 +8,19 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "d88d8fd17aab50ee8868",
-    "url": "/static/js/main.d88d8fd1.chunk.js"
+    "revision": "fb603dd1ea7949246e15",
+    "url": "/static/js/main.fb603dd1.chunk.js"
   },
   {
     "revision": "549149764a4abda863bc",
     "url": "/static/js/1.54914976.chunk.js"
   },
   {
-    "revision": "d88d8fd17aab50ee8868",
+    "revision": "fb603dd1ea7949246e15",
     "url": "/static/css/main.41e220fc.chunk.css"
   },
   {
-    "revision": "312afc9e2216b4e0cf56b6106975ffe3",
+    "revision": "d4ba3aac4b112a115af9c7e4a6d1e645",
     "url": "/index.html"
   }
 ];
